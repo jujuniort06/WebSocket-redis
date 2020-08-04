7980ffb530b05c0e9313ee0da2d1e2e621ec6423
Julio <julio_thomazelli-ju1@estudante.sc.senai.br>
2020-08-03 23:31:41 -0300

    Comandos GET, SET, DEL do REDIS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import { RedisClient } from "./redisclient";
 
 const redis = new RedisClient();
+const port = 6379;
+const ip   = '127.0.0.1';
 
-redis.getTeste();
+redis.connect(port, ip, () => {
+  console.log('Conectou');
+  redis.set('nome2', 'naiara');
+});
+
+setTimeout(() => {
+  redis.get('nome2');
+}, 2000);
+
+setTimeout(() => {
+  redis.del('nome2');
+}, 2000);
--- src/redisclient.ts
+++ src/redisclient.ts
@@ -1,5 +1,39 @@
-export class RedisClient{
-  public getTeste(){
-    console.log('Teste lixi');
+import { Socket } from "net";
+const RedisServer = require('redis-server');
+
+const net = require('net');
+
+export class RedisClient{   
+  
+  private server    : Socket;
+
+  constructor(){
+    this.server = new net.Socket();
+  }
+    
+  public connect(port : number, ip : string, callback : any){
+    this.server.connect(port, ip, () =>{
+      callback();
+    })   
+
+    this.server.on('data', (data) => {  
+      console.log(data.toString());
+    });
+
+    this.server.on('close', () => {
+      console.log('Fechou a conexão');
+    })
+  }
+
+  public get(key : string){
+    this.server.write('*2\r\n$3\r\nGET\r\n$' + key.length + '\r\n'+ key +'\r\n');
+  }
+
+  public set(key : string, value : string){
+    this.server.write('*3\r\n$3\r\nSET\r\n$' + key.length + '\r\n'+ key +'\r\n$'+ value.length +'\r\n'+ value +'\r\n');
+  }
+
+  public del(key : string){
+    this.server.write('*2\r\n$3\r\nDEL\r\n$' + key.length + '\r\n'+ key +'\r\n');
   }
 }
